f6a2800a5cc24e285ad8c3818412fad8f5f2f679
Aaron Clark <aaronpclark92@gmail.com>
2020-02-17 16:26:38 -0600

    cleared comments and print statments from selection_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,17 +1,12 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
+
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # print(f'outer loop -- smallest_index = {smallest_index}')
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
         for j in range(i+1, len(arr)):
-            # print(f'inner loop | j = {j} || arr[j] = {arr[j]}')
             if arr[j] < arr[i] and arr[j] < arr[smallest_index]:
-                # print(
-                #     f'{arr[j]} is less than {arr[i]} and is also less than {arr[smallest_index]}')
+
                 smallest_index = j
                 smallest_val = arr[smallest_index]
                 swap_val = arr[cur_index]
